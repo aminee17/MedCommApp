10dfe8e21dc7bd3f4417c3457103747062f65979
aminee17 <amine.moussa17357@gmail.com>
2025-10-14 11:01:16 +0100

    berassmi01

--- src/main/java/com/na/medical_mobile_app/config/SimpleCorsFilter.java
+++ src/main/java/com/na/medical_mobile_app/config/SimpleCorsFilter.java
@@ -36,8 +36,8 @@ public class SimpleCorsFilter implements Filter {
             response.setHeader("Access-Control-Allow-Origin", origin);
         }
         
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "3600");
         
