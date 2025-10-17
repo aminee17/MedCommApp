3fa66689fb1405dcd175831ba7fda0c7760e3079
aminee17 <amine.moussa17357@gmail.com>
2025-10-17 13:47:42 +0100

    last5

--- src/main/java/com/na/medical_mobile_app/controllers/NeurologueController.java
+++ src/main/java/com/na/medical_mobile_app/controllers/NeurologueController.java
@@ -15,7 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.stream.Collectors;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
