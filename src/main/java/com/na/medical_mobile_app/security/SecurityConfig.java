5cde8bd857b913d07dc292fd6e6f8b573549a09f
aminee17 <amine.moussa17357@gmail.com>
2025-11-26 12:30:05 +0100

    logfix

--- src/main/java/com/na/medical_mobile_app/security/SecurityConfig.java
+++ src/main/java/com/na/medical_mobile_app/security/SecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -27,11 +25,6 @@ public class SecurityConfig {
     
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
     
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
